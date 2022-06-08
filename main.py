ad47679f7420526207bf6710f2620633a150aa34
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-06-08 21:12:30 +0800

    refactor: _app to app

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from app import create_app
 
-_app = create_app()
+app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        app="main:_app",
+        app="main:app",
         host="0.0.0.0",
         port=5000,
         reload=True,
         debug=True,
-        # ssl_keyfile='192.168.1.232+3-key.pem',
-        # ssl_certfile='192.168.1.232+3.pem'
     )
