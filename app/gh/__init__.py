78a3ffde6a975b6c751bf0801f2e316adda236b0
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-06-10 18:41:10 +0800

    feat(gh): add check max cache

--- app/gh/__init__.py
+++ app/gh/__init__.py
@@ -14,6 +14,16 @@ cache_dir = Path(settings.cache_dir)
 cache_dir.mkdir(parents=True, exist_ok=True)
 
 
+def get_cache_size():
+    cache_size = 0
+    for file in cache_dir.iterdir():
+        cache_size += file.stat().st_size
+    return cache_size
+
+
+cache_size = get_cache_size()
+
+
 @router.get("/{github_path:path}")
 async def get_gh(request: Request, github_path: str, token: str | None = None):
     logger.info(
@@ -24,18 +34,35 @@ async def get_gh(request: Request, github_path: str, token: str | None = None):
             f"IP Address: {request.headers.get('X-Forwarded-For',request.client.host)} - request url: {request.url} - headers:{request.headers}"
         )
         raise HTTPException(status_code=404, detail="not found")
+
     filepath = cache_dir / github_path.replace("/", "_")
+    global cache_size
     try:
         stat = filepath.stat()
         if time.time() - stat.st_ctime <= settings.cache_time:
             async with aiofiles.open(filepath, "rb") as f:
                 return Response(content=await f.read())
+        cache_size -= filepath.stat().st_size
         logger.info(f"{github_path} cache has expired")
     except FileNotFoundError:
         pass
-    url = "https://raw.githubusercontent.com/" + github_path
 
+    url = "https://raw.githubusercontent.com/" + github_path
     response = await client.get(url, timeout=5)
+    cache_size += (
+        int(response.headers["content-length"])
+        if response.headers.get("content-encoding") != "gzip"
+        else len(response.content)
+    )
+    if cache_size > settings.max_cache:
+        files = [file for file in cache_dir.iterdir() if file != filepath]
+        files.sort(key=lambda file: file.stat().st_ctime)
+        for file in files:
+            cache_size -= file.stat().st_size
+            file.unlink()
+            logger.info(f"{file} has been deleted")
+            if cache_size <= settings.max_cache:
+                break
     async with aiofiles.open(filepath, "wb") as f:
         await f.write(response.content)
     return Response(content=response.content)
