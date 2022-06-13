8dae112558b02abdaf4d77ba1b3673f6572611dd
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-06-13 15:36:56 +0800

    refactor: del mount func

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from . import gh
 def create_app():
     app = FastAPI(openapi_url=settings.openapi_url)
     app.add_event_handler("shutdown", close_httpx_client)
-    mount(app)
+    app.include_router(gh.router, prefix="/gh")
 
     @app.get("/healthcheck")
     async def healthcheck():
@@ -18,8 +18,3 @@ def create_app():
 
 async def close_httpx_client():
     await gh.client.aclose()
-
-
-def mount(app: FastAPI):
-
-    app.include_router(gh.router, prefix="/gh")
