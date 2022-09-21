935332cb8c3ef3307fb6ce84b85a1f943fa13d53
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-09-21 17:59:36 +0800

    fix plain mess

--- app/gh/__init__.py
+++ app/gh/__init__.py
@@ -11,17 +11,61 @@ from ..logger import logger
 router = APIRouter()
 cache_dir = settings.cache_dir
 cache_dir.mkdir(parents=True, exist_ok=True)
+client = httpx.AsyncClient(base_url="https://raw.githubusercontent.com", timeout=5)
 
 
 def get_cache_size():
     cache_size = 0
     for file in cache_dir.iterdir():
-        cache_size += file.stat().st_size
+        if file.suffix != ".type":
+            cache_size += file.stat().st_size
     return cache_size
 
 
 cache_size = get_cache_size()
-client = httpx.AsyncClient(base_url="https://raw.githubusercontent.com", timeout=5)
+
+
+def get_cache_files(sort="st_ctime"):
+    files = [file for file in cache_dir.iterdir() if file.suffix != ".type"]
+    if sort == "st_ctime":
+        files.sort(key=lambda file: file.stat().st_ctime)
+    elif sort == "st_size":
+        files.sort(key=lambda file: file.stat().st_size)
+    return files
+
+
+def rm_cache_file(file):
+    file.unlink()
+    file.with_suffix(file.suffix + ".type").unlink()
+    logger.info(f"{file} has been deleted")
+
+
+def rm_cache_files(filepath=None):
+    def cache_files(sort="st_ctime"):
+        files = get_cache_files(sort=sort)
+        if filepath:
+            try:
+                files.remove(filepath)
+            except ValueError:
+                pass
+        return files
+
+    global cache_size
+
+    files = cache_files()
+    for file in files:
+        if time.time() - file.stat().st_ctime <= settings.cache_time:
+            break
+        cache_size -= file.stat().st_size
+        rm_cache_file(file)
+        if cache_size <= settings.max_cache:
+            return
+    files = cache_files(sort="st_size")
+    for file in files:
+        cache_size -= file.stat().st_size
+        rm_cache_file(file)
+        if cache_size <= settings.max_cache:
+            return
 
 
 @router.get("/{github_path:path}")
@@ -36,35 +80,38 @@ async def get_gh(request: Request, github_path: str, token: str | None = None):
         raise HTTPException(status_code=404, detail="not found")
 
     filepath = cache_dir / github_path.replace("/", "_")
+    typepath = filepath.with_suffix(filepath.suffix + ".type")
+
+    headers = {}
+    content = b""
+
     global cache_size
     try:
         stat = filepath.stat()
         if time.time() - stat.st_ctime <= settings.cache_time:
             async with aiofiles.open(filepath, "rb") as f:
-                return Response(content=await f.read())
-        cache_size -= filepath.stat().st_size
-        logger.info(f"{github_path} cache has expired")
+                content = await f.read()
+            async with aiofiles.open(typepath, "r") as f:
+                headers["content-type"] = await f.read()
+        else:
+            cache_size -= stat.st_size
+            logger.info(f"{github_path} cache has expired")
+            raise FileNotFoundError("cache has expired")
     except FileNotFoundError:
-        pass
-
-    response = await client.get(github_path)
-    file_size = (
-        int(response.headers["content-length"])
-        if response.headers.get("content-encoding") != "gzip"
-        else len(response.content)
-    )
-    if file_size > settings.file_max:
-        return Response(content=response.content)
-    cache_size += file_size
-    if cache_size > settings.max_cache:
-        files = [file for file in cache_dir.iterdir() if file != filepath]
-        files.sort(key=lambda file: file.stat().st_ctime)
-        for file in files:
-            cache_size -= file.stat().st_size
-            file.unlink()
-            logger.info(f"{file} has been deleted")
-            if cache_size <= settings.max_cache:
-                break
-    async with aiofiles.open(filepath, "wb") as f:
-        await f.write(response.content)
-    return Response(content=response.content)
+        response = await client.get(github_path)
+        file_size = (
+            int(response.headers["content-length"])
+            if response.headers.get("content-encoding") != "gzip"
+            else len(response.content)
+        )
+        if file_size <= settings.file_max:
+            cache_size += file_size
+            if cache_size > settings.max_cache:
+                rm_cache_files(filepath=filepath)
+            async with aiofiles.open(filepath, "wb") as f:
+                await f.write(response.content)
+            async with aiofiles.open(typepath, "w") as f:
+                await f.write(response.headers["content-type"])
+        content = response.content
+        headers["content-type"] = response.headers["content-type"]
+    return Response(content=content, headers=headers)
