b99065f866ea59f9ade740f2127d69484f3263f5
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-04-20 19:13:32 +0800

    gracefully shutting down background task

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::sync::Arc;
 
 use chrono::{Local, SecondsFormat};
 use dotenvy::dotenv;
+use tokio::{
+    signal,
+    sync::oneshot,
+    task::{spawn, AbortHandle},
+};
 use warp::{http::StatusCode, Filter};
 
 #[macro_use]
@@ -19,14 +24,24 @@ async fn main() {
     dotenv().ok();
     logger::init_logger();
     let log = logger::warp_log_custom("simple-gh");
-    let task_jh = task::background_task().await;
+    let (task_jh, task_cancel) = task::init_background_task();
     let client = reqwest::Client::new();
     let gh_routes = gh::routes(client.clone()).with(log);
-    let routes = alive_routes(Arc::new(task_jh)).or(warp::path("gh").and(gh_routes));
-    warp::serve(routes).run(config::CONFIG.addr).await;
+    let routes = alive_routes(Arc::new(task_jh.abort_handle())).or(warp::path("gh").and(gh_routes));
+    let (tx, rx) = oneshot::channel();
+    let (_, server) = warp::serve(routes).bind_with_graceful_shutdown(config::CONFIG.addr, async {
+        info!("listening on http://{}", config::CONFIG.addr);
+        rx.await.ok();
+        info!("SIGINT received; starting forced shutdown");
+    });
+    spawn(server);
+    signal::ctrl_c().await.unwrap();
+    let _ = tx.send(());
+    task_cancel.cancel();
+    task_jh.await.unwrap();
 }
 
-type Task = Arc<tokio::task::JoinHandle<()>>;
+type Task = Arc<AbortHandle>;
 
 fn alive_routes(
     task_jh: Task,
