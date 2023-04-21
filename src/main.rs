6139e97e55baeaa46b2f204d6cf8bc4b5bc6e6cd
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-04-21 20:55:34 +0800

    feat: config

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::net::SocketAddr;
-
 use axum::{routing::get, Router};
 use chrono::{Local, SecondsFormat};
 
+mod config;
+
 #[tokio::main]
 async fn main() {
+    launch_info();
     let app = Router::new().route("/alive", get(alive));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
-    axum::Server::bind(&addr)
+    axum::Server::bind(&config::CONFIG.addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
@@ -15,4 +15,10 @@ async fn main() {
 
 async fn alive() -> String {
     Local::now().to_rfc3339_opts(SecondsFormat::Millis, false)
+}
+
+fn launch_info() {
+    println!();
+    println!("=================== Starting Simple-Gh ===================");
+    println!();
 }
