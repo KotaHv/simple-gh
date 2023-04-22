11979715179d57ce6d33a2c729ab8828dcab86b0
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-04-23 01:04:06 +0800

    feat: Graceful Shutdown

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::sync::Arc;
 
 use axum::{middleware, routing::get, Router};
 use chrono::{Local, SecondsFormat};
+use tokio::{
+    signal::{
+        ctrl_c,
+        unix::{signal, SignalKind},
+    },
+    sync::oneshot,
+};
 
 #[macro_use]
 extern crate log;
@@ -13,22 +20,45 @@ mod task;
 mod util;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     launch_info();
     logger::init_logger();
     let client = Arc::new(reqwest::Client::new());
     let (task_jh, task_cancel) = task::init_background_task();
+    let (tx, rx) = oneshot::channel();
     let app = Router::new()
         .route("/alive", get(alive))
         .nest("/gh", gh::routes(client.clone()))
         .layer(middleware::from_fn(logger::logger_middleware));
 
-    axum::Server::bind(&config::CONFIG.addr)
+    let graceful = axum::Server::bind(&config::CONFIG.addr)
         .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
-        .await
-        .unwrap();
+        .with_graceful_shutdown(async {
+            info!("listening on http://{}", config::CONFIG.addr);
+            rx.await.ok();
+        });
+    tokio::spawn(graceful);
+    let mut sigterm = signal(SignalKind::terminate())?;
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigquit = signal(SignalKind::quit())?;
+    tokio::select! {
+        _ = sigint.recv() => {
+            info!("SIGINT received; starting forced shutdown");
+        },
+        _ = sigterm.recv() => {
+            info!("SIGTERM received; starting graceful shutdown");
+        },
+        _ = sigquit.recv() => {
+            info!("SIGQUIT received; starting forced shutdown");
+        },
+        _ = ctrl_c() => {
+            info!("SIGINT received; starting forced shutdown");
+        }
+    }
+    let _ = tx.send(());
     task_cancel.cancel();
     task_jh.await.unwrap();
+    Ok(())
 }
 
 async fn alive() -> String {
