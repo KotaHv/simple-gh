b2495ada2e2c920a2d2512ecdc1c87185d796fe4
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-04-23 01:37:06 +0800

    refactor: Graceful Shutdown

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,9 @@ use std::sync::Arc;
 
 use axum::{middleware, routing::get, Router};
 use chrono::{Local, SecondsFormat};
-use tokio::{
-    signal::{
-        ctrl_c,
-        unix::{signal, SignalKind},
-    },
-    sync::oneshot,
+use tokio::signal::{
+    ctrl_c,
+    unix::{signal, SignalKind},
 };
 
 #[macro_use]
@@ -20,24 +17,28 @@ mod task;
 mod util;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     launch_info();
     logger::init_logger();
+    info!("listening on http://{}", config::CONFIG.addr);
     let client = Arc::new(reqwest::Client::new());
     let (task_jh, task_cancel) = task::init_background_task();
-    let (tx, rx) = oneshot::channel();
     let app = Router::new()
         .route("/alive", get(alive))
         .nest("/gh", gh::routes(client.clone()))
         .layer(middleware::from_fn(logger::logger_middleware));
 
-    let graceful = axum::Server::bind(&config::CONFIG.addr)
-        .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
-        .with_graceful_shutdown(async {
-            info!("listening on http://{}", config::CONFIG.addr);
-            rx.await.ok();
-        });
-    tokio::spawn(graceful);
+    let server = axum::Server::bind(&config::CONFIG.addr)
+        .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>());
+    tokio::select! {
+        _ = server => {},
+        _ = shutdown_signal() => {}
+    }
+    task_cancel.cancel();
+    task_jh.await.unwrap();
+}
+
+async fn shutdown_signal() -> std::io::Result<()> {
     let mut sigterm = signal(SignalKind::terminate())?;
     let mut sigint = signal(SignalKind::interrupt())?;
     let mut sigquit = signal(SignalKind::quit())?;
@@ -55,9 +56,6 @@ async fn main() -> std::io::Result<()> {
             info!("SIGINT received; starting forced shutdown");
         }
     }
-    let _ = tx.send(());
-    task_cancel.cancel();
-    task_jh.await.unwrap();
     Ok(())
 }
 
